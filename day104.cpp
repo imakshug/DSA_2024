0ea6f47998d3889acf4278139bbd96a658057ff8
imakshug <akarni213gmail.com>
2024-06-08 20:45:28 +0530

    day 104

---
+++ day104.cpp
@@ -0,0 +1,45 @@
+// Longest Palindromic Substring
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 0) return "";
+
+        // Initialize a 2D dp vector with false
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start = 0, maxLength = 1;
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for substrings of length 3 or more
+        for (int length = 3; length <= n; ++length) {
+            for (int i = 0; i < n - length + 1; ++i) {
+                int j = i + length - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLength = length;
+                }
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
