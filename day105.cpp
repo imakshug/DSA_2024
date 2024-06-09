be6ca460d5e2f285ab6e9c718fb56c1d966681c6
imakshug <akarni213gmail.com>
2024-06-09 14:38:09 +0530

    day 105

--- day105.cpp
+++ day105.cpp
@@ -1,2 +1,27 @@
 //  Subarray Sums Divisible by K
 
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+
+        unordered_map<int,int>m;
+        int j=0;
+        int sum=0,ans=0;
+        while(j<nums.size())
+        {
+            sum+=nums[j];
+            if(sum%k==0)
+             ans++;
+            if(m.find((sum%k+k)%k)!=m.end())
+            ans+=m[(sum%k+k)%k];
+             m[(sum%k+k)%k]++;
+            j++;
+            
+        }
+        return ans;
+        
+    }
+};
