aaab3b8ba84a4fc73083d084d4f2d7cae6576406
imakshug <akarni213gmail.com>
2024-06-10 15:17:17 +0530

    day 106

---
+++ day106.py
@@ -0,0 +1,13 @@
+#   Height Checker
+
+from typing import List
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected=sorted(heights)
+        count=0
+        for i in range(len(heights)):
+            if expected[i]!=heights[i]:
+                count+=1
+        return count
+
+
