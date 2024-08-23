2cd184b47ef58b79c9d26bead89684b47c9793d9
Akshita Gupta <89334182+imakshug@users.noreply.github.com>
2024-08-23 12:28:54 +0530

    Update day179.cpp

--- day179.cpp
+++ day179.cpp
@@ -1,1 +1,22 @@
-// day 
+// number compliment
+
+class Solution {
+public:
+    int findComplement(int num) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int m = num;
+        int mask = 0;
+        if (num == 0) {
+            return 1;
+        }
+        while (m != 0) {
+            mask = (mask << 1) | 1;
+            m = m >> 1;
+        }
+        int ans = (~num) & mask;
+        return ans;
+    }
+};
+
