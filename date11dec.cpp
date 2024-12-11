af18bf3d0eb41f4f47151bcfa1985110350c312f
Akshita Gupta <89334182+imakshug@users.noreply.github.com>
2024-12-12 00:15:10 +0530

    Create date11dec.cpp

---
+++ date11dec.cpp
@@ -0,0 +1,21 @@
+//2779. Maximum Beauty of an Array After Applying Operation
+
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort (nums.begin(),nums.end());
+        int i=0;
+        int j=0;
+        int count=0;
+        while(j<nums.size()){
+            if ((nums[i]+k) >=(nums[j]-k)){
+                count=max(count,j-i+1);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return count;
+    }
+};
