37830ee16f2c9bc5bfe8ce8890ced69107d1d569
Akshita Gupta <89334182+imakshug@users.noreply.github.com>
2024-12-12 22:58:37 +0530

    Create date12dec.cpp

---
+++ date12dec.cpp
@@ -0,0 +1,21 @@
+//2558. Take Gifts From the Richest Pile
+
+class Solution {
+public:
+    long long int pickGifts(
+        std::vector<int> & gifts,
+        int seconds
+    ) const {
+        auto heap{std::priority_queue{std::less(), std::move(gifts)}};
+        while(seconds-- > 0) {
+            heap.push(std::floor(std::sqrt(heap.top())));
+            heap.pop();
+        }
+        auto sum{0LL};
+        while(!heap.empty()) {
+            sum += heap.top();
+            heap.pop();
+        }
+        return sum;
+    }
+};
